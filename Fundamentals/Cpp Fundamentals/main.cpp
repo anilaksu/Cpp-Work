0f2841fb0cbc7fbf7352bf3f9367fd5e38914f13
Anil Aksu <aaa293@cornell.edu>
2024-02-05 15:37:58 +0300

    Arithmetic Operators in C++
    Built-in arithmetic operations in C++

--- Fundamentals/Cpp Fundamentals/main.cpp
+++ Fundamentals/Cpp Fundamentals/main.cpp
@@ -1,3 +1,8 @@
+/*
+	Fundamentals of C++ version 17
+*/
+
+
 #include <iostream>
 #include <string>	 // Here we include string library for built-in string operations
 using namespace std; // Here we just say "include standard names"
@@ -8,17 +13,17 @@ int main()
 	cout << "Hello C++" << endl;
 
 	/*
-	- Storing different types of data in memory
-	- Performing operations on the data
-	- Taking our first steps toward helping our programs make decisions
-	- Communicating with the users of our programs
-	- Projects
-		1) Mad Libs clone
-		2) Average-of-Three program
+		- Storing different types of data in memory
+		- Performing operations on the data
+		- Taking our first steps toward helping our programs make decisions
+		- Communicating with the users of our programs
+		- Projects
+			1) Mad Libs clone
+			2) Average-of-Three program
 	*/
 
 	/*
-	Most develop assigne variable names using Camel Case convention
+		Most develop assigne variable names using Camel Case convention
 	*/
 	int myInt;			       // We declared our integer variable here
 	myInt = 15;				   // We assigned a value to it
@@ -32,15 +37,24 @@ int main()
 	cout << singleChar << endl;
 	cout << "Hello " << myName << endl;
 	cout << isRaining << endl;				// Here we output integer value of boolean variable 
+
 	// boolapha is also a part of iostream library that enables to output boolean variable as True or False
 	cout << boolalpha << isRaining << endl; // Here we output True or False of boolean variable
 
 	// Some Experiment on Boolean operator
-	bool isSunny = true;
+	bool isSunny = false;
 	bool isWarm = true;
 
 	cout << isSunny << endl;
 	cout << isWarm << endl;
+
+	/*
+		Arithmetic Operators:
+		- Operators: Special symbols used to perform operations
+		- Operands: The variables or literals that the opertors act upon
+		- Arithmetic operators: Used to perform arithmetic
+	*/
+
 
 	return 0;
 }
