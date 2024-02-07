dc424ec34150071a75873c9db9038286a2040e59
Anil Aksu <aaa293@cornell.edu>
2024-02-07 16:01:46 +0300

    Updated Selection and Repetition Statements
    Here I added samples of fundamental selection and repetition statements

--- Control Statements/Control Statements/main.cpp
+++ Control Statements/Control Statements/main.cpp
@@ -33,5 +33,162 @@ int main()
 		cout << "You can not drive yet!" << endl;
 	}
 
+	// for loop (repetition)
+	for (int i = 1; i <= age; i++)
+	{
+		cout << "Happy birthday " << i << endl;
+	}
+
+	/*
+		Selection Control Statements Deeper Dive:
+		- if
+		- if - else
+		- switch
+	*/
+
+	cout << "Welcome to the Pub and Grille!" << endl;
+	cout << "Please enter your age" << endl;
+	cin >> age;
+
+	if (age >= 21) {
+		cout << "Here have a beer" << endl;
+	}
+	else if (age >= 16) {
+		cout << "Heve have a coke!" << endl;
+		cout << "And, at least you can drive!" << endl;
+	}
+	else
+	{
+		cout << "Here have a coke" << endl;
+	}
+
+	cout << "Thanks for coming to the Pub and Grille!" << endl;
+
+	// Switch Statement
+
+	char grade;
+
+	cout << "Please enter a letter grade" << endl;
+	cin >> grade;
+
+	switch (grade)
+	{
+		case 'A':
+		case 'a':
+			cout << "Great job!" << endl;
+			break;
+		case 'B':
+		case 'b':
+			cout << "Good job!" << endl;
+			break;
+		case 'C':
+		case 'c':
+			cout << "You can do better!" << endl;
+			break;
+		case 'D':
+		case 'd':
+			cout << "You are getting close to failing!" << endl;
+			break;
+		case 'F':
+		case 'f':
+			cout << "You are failing the course!" << endl;
+			break;
+		default:
+			cout << "You have entered an invalid grade. Try again" << endl;
+	}// end switch
+
+	//prompt user for age and gender
+	//age is int, gender is character
+	//get of f or F and are 60 or over
+
+	char gender;
+
+	cout << "Please enter your gender (F/M)" << endl;
+	cin >> gender;
+
+	cout << "Please enter your age" << endl;
+	cin >> age;
+
+	if ((gender == 'F' || gender == 'f') && age >= 60) {
+		cout << "You are eligible for the discount" << endl;
+	}
+	else {
+		cout << "You are not eligible for the discount" << endl;
+	}
+
+	/*
+		Repetition Control Statements Deeper Dive:
+		- while loops
+		- do - while loops
+		- for loops
+	*/
+
+	int count = 0;
+
+	cout << "While loop" << endl;
+
+	while (count < 10) {
+
+		cout << count << endl;
+		count++;
+	}
+
+	cout << "Do while loop" << endl;
+
+	// do while executes then checks the condition
+	do
+	{
+		cout << count << endl;
+		count--;
+	} while (count > 0);
+
+	cout << "For loop" << endl;
+	// for loop
+	for (int i = 0; i < 10; i++) {
+		cout << i << endl;
+	}
+
+	/*
+		Break and Continue:
+		- Break: early exit from a repetition control statement
+		- Continue: causes an iteration of a repetition control statement to be skipped 
+	*/
+
+	cout << "Continue statement" << endl;
+	while (count < 10)
+	{	
+		// Here we say skip when count == 5
+		if (count == 5) { 
+			count++;
+			continue;
+		}
+		cout << count << endl;
+		count++;
+	}
+
+	cout << "Break statement" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		// Here we say break the loop when i == 5
+		if (i == 5) {
+			break;
+		}
+		cout << i << endl;
+	}
+
+	// Print only odd integers using CONTINUE statement from 1 to 10
+	cout << "Print only odd numbers" << endl;
+	for (int i = 1; i <= 10; i++) {
+		// Here we say skip the loop when i is even
+		if (i % 2 == 0) {
+			continue;
+		}
+		cout << i << endl;
+	}
+
+	/*
+		Random Numbers
+	*/
+
 	return 0;
 }
