4605cd13e9bf245304ec02e719c856d90aab5865
Anil Aksu <aaa293@cornell.edu>
2024-02-12 09:47:49 +0300

    Parameter Passing in C++
    Here I show examples of how to pass-by-value and pass-by-reference in functions.

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -23,6 +23,10 @@ void printMyName(string yourName); // Function Protype
 
 int giveMe10();
 int addThese(int num1, int num2);
+int square(int num1);
+void valueChanged1(int someNum);			// It changes pass-by-value
+void valueChanged2(int& someNum);			// It changes pass-by-reference
+void threeTimesN(int input, int& output);	// It uses both pass-by-value and pass-by-reference
 
 int main() {
 
@@ -34,14 +38,62 @@ int main() {
 	getline(cin, myName);
 
 	printMyName(myName);
-
 	cout << giveMe10() << endl;
 	cout << "Your sum is " << addThese(num1, num2) << endl;
-
+	cout << "Your square is " << square(num1) << endl;
 
 	/*
 		Parameter Passing: Pass-by-Value and Pass-by-Reference
+			- Pass-by-value
+				+ The default parameter passing scheme
+				+ Think of it as "pass-by-copy"
+			- Pass-by-reference
+				+ Allows us to give more control to the function
+				+ The parameter acts as an aliase for the variable from the calling function
+				+ Values of parameters passed by reference that are changed reflect 
+				the change in the change in the argument that was passed to them
 	*/
+
+	int myNumber = 20;
+
+	/*
+		When we pass-by-value, it does not change the number
+	*/
+	cout << " Pass-by-value " << endl;
+	cout << "Before valueChange1 call, my number is " << myNumber << endl;
+	valueChanged1(myNumber);
+	cout << "After valueChange1 call, my number is " << myNumber << endl;
+
+	/*
+		When we pass-by-reference, it does change the number
+	*/
+
+	cout << " Pass-by-reference " << endl;
+	cout << "Before valueChange2 call, my number is " << myNumber << endl;
+	valueChanged2(myNumber);
+	cout << "After valueChange2 call, my number is " << myNumber << endl;
+
+
+	/*
+		Parameter Passing Challenge:
+			- Add a function called threeTimesN
+			- The function is a void function takes two integer parameters
+			- Parameters:
+				+ First paramenter, input is an integer passed by value
+				+ Second parameter, output is an integer passed by reference
+			- Store the input multiplied by 3 intothe output variable
+	*/
+
+	/* 
+		Variable Scope and Lifetime
+	*/
+
+	cout << "Please enter an input for threeTimesN function" << endl;
+	cin >> num1;
+
+	threeTimesN(num1, num2); // Here we expect to change num2
+	 
+	cout << "Your output by threeTimesN function is " << num2 << endl;
 
 	return 0;
 }
@@ -67,4 +119,27 @@ int giveMe10()
 int addThese(int num1, int num2)
 {
 	return num1 + num2;
+}
+
+int square(int num1)
+{
+	return num1 * num1;
+}
+
+// Pass-by-value value change function
+void valueChanged1(int someNum)
+{
+	someNum = 100;
+}
+
+// Pass-by-reference value change function
+void valueChanged2(int& someNum)
+{
+	someNum = 100;
+}
+
+// Both Pass-by-value and Pass-by-reference function
+void threeTimesN(int input, int& output)
+{
+	output = 3 * input;
 }
