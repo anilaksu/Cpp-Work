2dc4f22f58cfbbb7b8506591f7c186b186348737
Anil Aksu <aaa293@cornell.edu>
2024-03-18 09:51:10 +0300

    Classes and Objects Review and Conventions

--- Classes and Objects/ClassObjects/Classes.h
+++ Classes and Objects/ClassObjects/Classes.h
@@ -1,192 +1,48 @@
 #pragma once
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-class Time {
+class House {
 	public:
-		Time() : hour(0), minute(0), second(0)
-		{	}
+		// Member functions
 
-		Time(int h, int m, int s) : hour(h), minute(m), second(s)
-		{	}
-
-		~Time()
-		{	}
-
-		void setTime(int h, int m, int s) {
-			hour = h;
-			minute = m;
-			second = s;
+		// Setter functions sets fields in private
+		void setNumStories(int numStories) {
+			this->numStories = numStories;    // this-> shows we want to use data member
 		}
 
-		void displayTime() {
-			cout << "Time is: " << hour << ":" << minute << ":" << second << endl;
+		void setNumWindows(int numWindows) {
+			this->numWindows = numWindows;
 		}
 
-		Time addTime(Time t) {
-			Time temp;
-			int seconds = second + t.second;
-			int minutes = minute + t.minute;
-			int hours = hour + t.hour;
-
-			/* let's add any extra minute by summation*/
-			minutes = minutes + int(seconds / 60);
-			seconds = seconds % 60;
-			/* let's add any extra hour by summation*/
-			hours = hours + int(minutes / 60);
-			minutes = minutes % 60;
-			/*Let's set our final time*/
-			temp.setTime(hours, minutes, seconds);
-			return temp;
-		}
-	private:
-		int hour, minute, second;
-};
-
-
-
-class toolBooth {
-
-	public:
-		toolBooth() : carnumber(0), money(0)
-		{}
-
-		void payingCar() {
-			carnumber++;
-			money = money + 0.5;
+		void setColor(string color) {
+			this->color = color;
 		}
 
-		void nonpayingCar() {
-			carnumber++;
-		}
-
-		void display() {
-			cout << "Total number of cars is " << carnumber << endl;
-			cout << "Total money collected is " << money << endl;
-		}
-	private:
-		int carnumber;
-		float money;
-};
-class Int {
-
-	public:
-		Int(): data(0)
-			{	}
-		Int(int d): data(d)
-			{	}
-		void setData(int d) {
-			data = d;
-		}
-
-		int getData() const {
-			return data;
-		}
-	private:
-		int data;
-};
-class foo {
-	private:
-		static int count;
-
-	public:
-		foo()
-		{ count++; }
-		int getcount()
+		// Getter functions access fields in private
+		int getNumStories() const // const indicates it accesses the data but can not change it
 		{
-			return count;
-		}
-};
-
-class Distance //English Distance class
-{
-private:
-	int feet;
-	float inches;
-
-public: //constructor (no args)
-	Distance() : feet(0), inches(0.0)
-	{ }
-	//constructor (two args)
-	Distance(int ft, float in) : feet(ft), inches(in)
-	{ }
-	void getdist() //get length from user
-	{
-		cout << "\nEnter feet : "; cin >> feet;
-		cout << "Enter inches : "; cin >> inches;
-	}
-	void showdist() const //display distance
-	{
-		cout << feet << "\' - " << inches << "\'";
-	}
-	void add_dist(Distance, Distance); //declaration
-	Distance add_dist(Distance); //declaration
-
-};
-
-
-void Distance::add_dist(Distance d2, Distance d3) 
-{
-	inches = d2.inches + d3.inches; //add the inches
-	feet = 0; //(for possible carry)
-	if (inches >= 12.0) //if total exceeds 12.0,
-	{ //then decrease inches
-		inches -= 12.0; //by 12.0 and
-		feet++; //increase feet
-	} //by 1
-	feet += d2.feet + d3.feet; //add the feet
-}
-
-Distance Distance::add_dist(Distance d2)
-{
-	Distance temp; //temporary variable
-	temp.inches = inches + d2.inches; //add the inches
-	if (temp.inches >= 12.0) //if total exceeds 12.0,
-	{ //then decrease inches
-		temp.inches -= 12.0; //by 12.0 and
-		temp.feet = 1; //increase feet
-	} //by 1
-	temp.feet += feet + d2.feet; //add the feet
-	return temp;
-}
-
-class part {
-	public:
-		part(): modelnumber(), partnumber(), cost() 
-			{	}
-		~part()
-			{	}
-		/* Here set part info*/
-		void setPart(int mn, int pn, float c) {
-			modelnumber = mn;
-			partnumber = pn;
-			cost = c;
+			return numStories;
 		}
 
-		void getPartInfo() const {
-			cout << "Part model number is: " << modelnumber << endl;
-			cout << "Part part number is: " << partnumber << endl;
-			cout << "Part cost is: " << cost << endl;
+		int getNumWindows() const
+		{
+			return numWindows;
 		}
-private:
-	/* Part info in private*/
-	int modelnumber;
-	int partnumber;
-	float cost;
 
-};
+		string getColor() const
+		{
+			return color;
+		}
+		
+	private: // Private member is only accessible within a function
+		// Data Members or Fields
+		int numStories;
+		int numWindows;
+		string color;
 
-class smallobj //define a class
-{
-private:
-	int somedata; //class data
-public:
-	void setdata(int d) //member function to set data
-	{
-		somedata = d;
-	}
-	void showdata() //member function to display data
-	{
-		cout << "Data is " << somedata << endl;
-	}
-};
+}; // end of the class House
+
+
--- Classes and Objects/ClassObjects/main.cpp
+++ Classes and Objects/ClassObjects/main.cpp
@@ -1,81 +1,44 @@
+/*
+	Classes and Objects in C++
+		- Unified Modeling Language (UML) help us to model our classes during software design
+*/
+
 #include <iostream>
-#include <string.h>
-#include <sstream>
-#include "cmath"
+#include <string>
 #include "Classes.h"
 
 using namespace std;
-#include <conio.h> //for getche()
-const int NumDir = 4;
-int foo::count = 0; //*definition* of count
-
-//void starline(); //function declaration
-// (prototype)
 
 int main()
 {
-	
-	smallobj s1, s2; //define two objects of class smallobj
-	part part1;      // define the part
-	Distance dist1, dist3; //define two lengths
-    const Distance dist2(11, 6.25); //define and initialize dist2
-	foo f1, f2, f3;
-	Int data1(2), data2(1), data3;
-	toolBooth toolbooth1;
-	string cartype = { "nonpaying" };
-	Time time1(9, 30, 33), time2(10, 35, 40), time3;
+	/*
+		Object-oriented programming encapsulates behaviours and data:
+			1) Encapsulation: 
+				- Secuirty: Information hiding where data is protected from outside world
+				- Portability: A class acts as a blueprint for an object
+			2) Inheritance:	 It will be explained in detailed!
+			3) Polymorphism: It will be explained in detailed! 
+	*/
 
+	House myHouse;
+	House yourHouse;
 
-	dist1.getdist(); //get dist1 from user
-	//dist3.add_dist(dist1, dist2); //dist3 = dist1 + dist2
-	dist3 = dist1.add_dist(dist2); //dist3 = dist1 + dist2
-	//display all lengths
+	// Here we set our houses properties using member functions
+	myHouse.setNumStories(2);
+	myHouse.setNumWindows(6);
+	myHouse.setColor("Red");
 
+	yourHouse.setNumStories(3);
+	yourHouse.setNumWindows(10);
+	yourHouse.setColor("Blue");
 
-	s1.setdata(1066); //call member function to set data
-	s2.setdata(1776);
+	cout << "My house is " << myHouse.getColor() << " and has "
+		<< myHouse.getNumStories() << " stories and " << myHouse.getNumWindows()
+		<< " windows. " << endl;
 
-	s1.showdata(); //call member function to display data
-	s2.showdata();
-
-	part1.setPart(210, 10, 520.25);
-	/* Let's output the part info */
-	part1.getPartInfo();
-
-	cout << "\ndist1 = "; dist1.showdist();
-	cout << "\ndist2 = "; dist2.showdist();
-	cout << "\ndist3 = "; dist3.showdist();
-	cout << endl;
-
-	cout << "count is " << f1.getcount() << endl; //each object
-	cout << "count is " << f2.getcount() << endl; //sees the
-	cout << "count is " << f3.getcount() << endl; //same value
-	cout << endl;
-	/*for (int i = 0; i < 20; i++)
-		callCounter();*/
-
-	/* Let's add two data*/
-	data3.setData(data2.getData() + data1.getData());
-	/* Let's output the data 3 */
-	cout << "Third data is: " << data3.getData() << endl;
-
-	while (cartype != "Esc") {
-		cout << "Enter Next Car Type" << endl;
-		cin >> cartype;
-		if (cartype == "paying")
-			toolbooth1.payingCar();
-		else if (cartype == "nonpaying")
-			toolbooth1.nonpayingCar();
-	}
-
-	/* Let's output the money we collected */
-	toolbooth1.display();
-
-	/*Let's add two time to calculate the last time*/
-	time3 = time1.addTime(time2);
-	/*Let's display the last time*/
-	time3.displayTime();
-
+	cout << "Your house is " << yourHouse.getColor() << " and has "
+		<< yourHouse.getNumStories() << " stories and " << yourHouse.getNumWindows()
+		<< " windows. " << endl;
 
 	return 0;
 }
