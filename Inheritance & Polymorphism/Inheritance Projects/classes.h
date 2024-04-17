a8c2146c965a63f9e5fc6007e46509047ba45f88
Anil Aksu <aaa293@cornell.edu>
2024-04-17 14:07:59 +0300

    Package Hierarchy Class completed, now added Account Hierarchy Class

--- Inheritance & Polymorphism/Inheritance Projects/classes.h
+++ Inheritance & Polymorphism/Inheritance Projects/classes.h
@@ -86,7 +86,7 @@ class Package
 {
 public:
 	Package(string senderName, string recipientName, string senderAddress, string recipientAddress,
-		double weight, double costPerOunce); // Constructor for the base class Commision Employee
+		double weight, double costPerOunce); // Constructor for the base class Package
 
 	// Setter functions
 	void setSenderName(string senderName);
@@ -117,3 +117,162 @@ private:
 };
 
 #endif // !PACKAGE_H
+
+#ifndef TWODAY_H
+#define TWODAY_H
+
+class TwoDayPackage : public Package
+{
+public:
+	TwoDayPackage(string senderName, string recipientName, string senderAddress, string recipientAddress,
+		double weight, double costPerOunce, double flatFee) : Package(senderName, recipientName, senderAddress,
+			recipientAddress, weight, costPerOunce)
+	{
+		this->flatFee = flatFee;
+	}// Constructor for the der'ved class Two Day Package
+
+	// Setter functions
+	void setFlatFee(double flatFee)
+	{
+		this->flatFee = flatFee;
+	}
+	// Getter functions
+	double getFlatFee() const
+	{
+		return this->flatFee;
+	}
+
+	// Internal Operations using Class Data 
+	double totalCost()
+	{
+		return this->getCostPerOunce() * this->getWeight() + this->flatFee;
+	}
+
+private:
+	double flatFee;
+
+};
+
+
+#endif // End of Two Day Package Class
+
+#ifndef OVERNIGHT_H
+#define OVERNIGHT_H
+
+class OvernightPackage : public Package
+{
+public:
+	OvernightPackage(string senderName, string recipientName, string senderAddress, string recipientAddress,
+		double weight, double costPerOunce, double additionalFee) : Package(senderName, recipientName, senderAddress,
+			recipientAddress, weight, costPerOunce)
+	{
+		this->additionalFee = additionalFee;
+	}// Constructor for the derived class Overnight Package Package
+
+	// Setter functions
+	void setAdditionalFee(double additionalFee)
+	{
+		this->additionalFee = additionalFee;
+	}
+	// Getter functions
+	double getAdditionalFee() const
+	{
+		return this->additionalFee;
+	}
+
+	// Internal Operations using Class Data 
+	double totalCost()
+	{
+		return (this->getCostPerOunce() + this->additionalFee) * this->getWeight();
+	}
+
+private:
+	double additionalFee;
+
+};
+
+#endif // End of Overnight Package Class
+
+// Question from Chapter 11.10 (Account Inheritance Hierarchy)
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+class Account
+{
+public:
+	Account(double accountBalance)
+	{
+		try
+		{
+			processPositive(accountBalance);
+			this->accountBalance = accountBalance;
+			cout << "Your account has $" << this->accountBalance << " balance." << endl;
+		}
+		catch (const invalid_argument& err)
+		{
+			cout << err.what() << endl;
+			cin >> accountBalance; // Here we read the new account balance
+			setAccountBalance(accountBalance);
+		}
+		
+	}// Constructor for the base class Package
+
+	// Setter functions
+	void setAccountBalance(double accountBalance) {
+		try
+		{
+			processPositive(accountBalance);
+			this->accountBalance = accountBalance;
+			cout << "Your account has $" << this->accountBalance << " balance." << endl;
+		}
+		catch (const invalid_argument& err)
+		{
+			cout << err.what() << endl;
+			cin >> accountBalance; // Here we read the new account balance
+			setAccountBalance(accountBalance);
+		}
+	}
+
+	void processPositive(double accountBalance) // Throws exceptions
+	{
+		if (accountBalance >= 0.)
+		{
+			this->accountBalance = accountBalance;
+		}
+		else
+		{
+			throw invalid_argument("Your account can not have a negative balance!\n Please enter a positive balance");
+		}
+	}
+
+	// Getter functions
+	double getAccountBalance() const {
+		return this->accountBalance;
+	}
+
+	// Internal Operations using Class Data 
+	void debit(double debitAmount) {
+
+		double temporaryBalance = this->accountBalance - debitAmount;
+		try
+		{
+			processPositive(temporaryBalance);
+			this->accountBalance = temporaryBalance;
+		}
+		catch (const invalid_argument& err)
+		{
+			cout << err.what() << endl;
+			cin >> debitAmount; // Here we read the new account balance
+			debit(debitAmount);
+		}
+	}
+
+	void credit(double creditAmount) {
+		this->accountBalance = this->accountBalance + creditAmount;
+	}
+
+private:
+	double accountBalance;
+};
+
+#endif // !ACCOUNT_H
--- Inheritance & Polymorphism/Inheritance Projects/main.cpp
+++ Inheritance & Polymorphism/Inheritance Projects/main.cpp
@@ -20,10 +20,32 @@ int main()
 	cout << "Your base plus employee sold " << ((BasePlusCommssionEmployee*)basePlusPtr)->getGrossSales() 
 		<< " units and made $" << ((BasePlusCommssionEmployee*)basePlusPtr)->earnings() << endl;
 	cout << "Your base plus employee has $" << ((BasePlusCommssionEmployee*)basePlusPtr)->getBaseSalary() << " base salary." << endl;
+	cout << endl;
 
 	delete basePlusPtr;
 	basePlusPtr = nullptr;
 
+	OvernightPackage myOvernightPackage("Can Bilir", "Anil Aksu", "Cin Cin", "Seyran", 20.1, 0.1, 0.2);
+	TwoDayPackage myTwoDayPackage("Can Bilir", "Anil Aksu", "Cin Cin", "Seyran", 20.1, 0.1, 10.1);
+
+	cout << "If you choose overnight delivery, it costs $" << myOvernightPackage.totalCost() << endl;
+	cout << "If you choose two day delivery, it costs $" << myTwoDayPackage.totalCost() << endl;
+	cout << endl;
+
+
+	// Account Hierarchy
+	double credit, debit;
+	Account myAccount(-100.0);
+
+	cout << "How much would you like to debit from your account?" << endl;
+	cin >> debit;
+	myAccount.debit(debit);
+	cout << "Your account has $" << myAccount.getAccountBalance() << " balance." << endl;
+
+	cout << "How much would you like to credit to your account?" << endl;
+	cin >> credit;
+	myAccount.credit(credit);
+	cout << "Your account has $" << myAccount.getAccountBalance() << " balance." << endl;
 
 	return 0;
 }
